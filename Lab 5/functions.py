f54363e5215a9a9b68590f247b8fb0417be75091
eposri <kaylango1020@gmail.com>
2025-09-30 06:44:55 -0700

    lab5 c

--- Lab 5/functions.py
+++ Lab 5/functions.py
@@ -3,3 +3,146 @@ Name: Kayla Ngo
 Date: 09/17/2025
 File Purpose: holds function definitions and implementations
 '''
+
+# Dictionary Operations
+
+def create_dictionary(pairs):
+    '''Create a dictionary from a list of `(key, value)` tuples.'''
+    d = dict(pairs)
+    return d        # do i need this ?_?
+
+def update_dictionary(d, key, value):
+    '''
+    Update `d` with the given key and value. Overwrite if the key exists.
+    Return the updated dictionary.
+    '''
+    d[key] = value
+    return d
+
+def delete_key(d, key):
+    '''
+    Remove `key` from `d`. If `key` is not present, either return an error
+    message or raise an exception. Choose one approach and use it
+    consistently. Return the updated dictionary or the error message.
+    '''
+    if key not in d:
+        raise KeyError("Key does not exist.")
+    
+    d.pop(key)
+    return d
+
+def dict_comprehension_example(iterable):
+    '''
+    Return a dictionary that maps each item to its square if numeric,
+    or its length if a string.
+    '''
+    d = {}
+    
+    for item in iterable:
+        if type(item) == int or type(item) == float:
+            d[item] = item ** 2
+        else:
+            d[item] = len(item)
+    return d
+
+def merge_dictionaries(dicts):
+    '''
+    Merge a list of dictionaries into one dictionary. If the same key
+    appears more than once, collect all values for that key in a list.
+    Return the merged dictionary.
+    '''
+    merged_d = {}
+    
+    for d in dicts:
+        for k, v in d.items():
+            if k in merged_d:
+                merged_d[k].append(v)
+            else:
+                merged_d[k] = [v]
+    return merged_d
+
+
+# Looping Techniques
+
+def iterate_dictionary(d):
+    '''
+    Iterate over `d` and return a list of strings
+    formatted as `"key: value"`.
+    '''
+    li = []
+    
+    for k, v in d.items():
+        li.append(f"{k}: {v}")
+    return li
+
+def enumerate_list(lst):
+    '''Return a list of `(index, element)` tuples using `enumerate`.'''
+    li = []
+    
+    for i, e in enumerate(lst):
+        li.append((i, e))
+    return li
+
+def zip_lists(lst1, lst2):
+    '''
+    Pair elements from `lst1` and `lst2` with `zip` and
+    return a list of tuples.
+    '''
+    zipped_li = []
+    
+    for li_1, li_2 in zip(lst1, lst2):
+        zipped_li.append((li_1, li_2))
+    return zipped_li
+
+def reverse_and_sort(lst):
+    '''
+    Return a tuple with:
+
+    1. the reversed list
+    2. the sorted list
+    '''
+    return (reversed(lst), sorted(lst))
+
+
+# Conditions and Sequence Comparisons
+
+def check_membership(sequence, value):
+    '''Return `True` if `value` is in `sequence`, else `False`.'''
+    if value in sequence:
+        return True
+    return False
+
+def chained_comparison(a, b, c):
+    '''Evaluate `a < b == c`. Return the Boolean result.'''
+    return a < b == c
+
+def boolean_evaluation(a, b, c):
+    '''
+    Evaluate `(a and not b) or c` using Python short-circuit behaviour.
+    Return the result.
+    '''
+    return (a and not b) or c
+
+def compare_sequences(seq1, seq2):
+    '''
+    Compare `seq1` and `seq2` lexicographically. 
+    Return `-1` if `seq1 < seq2`,
+            `0` if equal,
+            `1` if `seq1 > seq2`.
+    '''
+    if seq1 < seq2:
+        return -2
+    elif seq1 == seq2:
+        return 0
+    else:
+        return 1
+
+def is_strictly_increasing(sequence):
+    '''
+    Return `True` if every element is less than the next element,
+    else `False`.
+    '''    
+    for i in range(len(sequence) - 1):
+        if sequence[i+1] <= sequence[i]:
+            return False
+    return True
--- Lab 5/main.py
+++ Lab 5/main.py
@@ -13,13 +13,13 @@ if __name__ == "__main__":
     print("c. Dictionary Operations")
     line()
 
-    # 1. create dictionary
+    # create dictionary
     pairs = [("a", 1), ("b", 2), ("c", 3)]
     print ("1. create_dictionary from pairs:", pairs)
     cd = create_dictionary(pairs)
-    print("create_dcitionary output: ", cd) 
+    print("create_dictionary output: ", cd) 
 
-    # 2. update dictionary
+    # update dictionary
     d1 = {"x": 1}
     print("update_dictionary input:", d1, "update('x', '100)")
     print("output:", update_dictionary(d1, "x", 100))
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     print("output:", update_dictionary(d1, "y", 2))
 
 
-    # 3. delete key
+    # delete key
     d2 = {"m": 10, "n": 20}
     print("delete_key start dict:", d2)
     print("delete_key remove 'm'")
@@ -38,13 +38,13 @@ if __name__ == "__main__":
     except KeyError as e:
         print("caught:", e)
 
-    #4 dictionary comprehension example
+    # dictionary comprehension example
     iterable = [2, 3.5, "cat", "go"]
     print("dict_comprehension_example input:", iterable)
     dce = dict_comprehension_example(iterable)
     print("output: ", dce)
 
-    #5 merge dictionaries
+    #= merge dictionaries
     dicts = [{"a": 1, "b": 2}, {"b": 3, "c": 4}, {"a": 5}]
     print("merge_dictoinaries input:", dicts)
     md = merge_dictionaries(dicts)
@@ -54,26 +54,26 @@ if __name__ == "__main__":
     print("D. Looping Techniques")
     line()
 
-    #6 iterate dictionary
+    # iterate dictionary
     d3 = {"name": "Ana", "age": 30}
     print("iterate_dictionary input:", d3)
     id_out = iterate_dictionary(d3)
     print("output: ", id_out)
 
-    #7 enumerate list
+    # enumerate list
     colours = ["red", "green", "blue"]
     print("enumerate_list input:", colours)
     el_out = enumerate_list(colours)
     print("output: ", el_out)
 
-    #8 zip lists
+    # zip lists
     lst1 = [1, 2, 3]
     lst2 = ["one", "two"]
     print("zip_lists inputs:", lst1, lst2)
     zl_out = zip_lists(lst1, lst2)
     print("output: ", zl_out)
 
-    #9 reverse and sorted
+    # reverse and sorted
     lst3 = [3, 1, 2]
     print("reverse_and_sort input:" , lst3)
     rev_list, sorted_list = reverse_and_sort(lst3)
@@ -83,20 +83,25 @@ if __name__ == "__main__":
     print("E. Condition and Sequence Comparisons")
     line()
 
+    # check membership
     seq = [1, 2, 3]
     print("Check_membership in", seq, "for 2:", check_membership (seq, 2))
     print("Check_membership in", seq, "for 4:", check_membership (seq, 4))
 
+    # chained comparison
     print("chained comparison 1 < 2 == 2", chained_comparison(1, 2, 3))
     print("chained comparison 3 < 2 == 2", chained_comparison(1, 2, 3))
 
+    # boolean evaluation
     print("boolean_evaluation True, False, False:", boolean_evaluation(True, False, False))
     print("boolean_evaluation 0, 0 'hello':", boolean_evaluation(0, 0, "hello"))
 
+    # compare sequences
     print("compare_sequences [1, 2] vs [1, 2, 3]:", compare_sequences([1, 2], [1, 2, 3]))
     print("compare_sequences [1, 2, 3] vs [1, 2, 3]:", compare_sequences([1, 2, 3], [1, 2, 3]))
     print("compare_sequences [2] vs [1, 9]:", compare_sequences([2], [1, 9]))
 
+    # is strictly increasing
     print("is_strictly_increasing [1, 2, 3]:", is_strictly_increasing([1, 2, 3]))
     print("is_strictly_increasing [1, 1, 2]:", is_strictly_increasing([1, 1, 2]))
     print("is_strictly_increasing [0, 0, 0]:", is_strictly_increasing([0, 0, 0]))
